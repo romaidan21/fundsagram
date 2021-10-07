3671e6605851c4b4c4af1102523389d0e7f44f45
Roman Maidan <romecom2210@gmail.com>
2021-10-07 18:10:39 +0300

    added js

--- src/desktop/js/scripts.js
+++ src/desktop/js/scripts.js
@@ -1,5 +1,22 @@
+document.addEventListener('DOMContentLoaded', e => {
+    const body = document.querySelector('body');
+    openPopUp(body, '.toggle-main', 'openMain', false)
+    openPopUp(body, '.open-form', 'openForm', 'openMain')
 
-document.getElementById('buttonClick').addEventListener('click', function () {
-    alert('sadasdasd') 
+    //зробити через об"єкт
+});
+
+
+function openPopUp(body, button, classAdd, classRemove) {
+    const buttons = document.querySelectorAll(button);
+
+    [...buttons].forEach(button =>{
+        button.addEventListener('click', openPP)
+    })
+
+    function openPP() {
+        if(classRemove) body.classList.remove(classRemove);
+        body.classList.toggle(classAdd);
+    }
     
-}) 
+}
